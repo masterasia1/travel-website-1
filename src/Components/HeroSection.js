783d1a8bea1d7da59cefd446a2f144f1f342ba18
masterasia1 <a.mayfield18@gmail.com>
2022-04-17 03:18:49 -0700

    added pages

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {Button} from './Button';
+import './HeroSection.css';
+import '../App.css'
+
+
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-        video src =''
+        <video src='../../public/videos/video.mp4' autoPlay loop muted />
+        <h1> A NEW LIFE AWAITS</h1>
+        <p>What are you waiting for?</p>
+        <div className='hero-btns'>
+          <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>Start Now</Button>
+          <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Look What You Can Do <i className='fa-play-circle' /></Button>
+
+
+        </div>
+        
 
     </div>
   )
---
+++ src/Components/pages/home.js
@@ -0,0 +1,15 @@
+import '../../App.css'
+import HeroSection from '../HeroSection'
+import React from 'react'
+
+function Home () {
+    return (
+        <>
+
+        <HeroSection />
+
+        </>
+    )
+}
+
+export default Home 
