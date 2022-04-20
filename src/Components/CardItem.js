5f067dcbc66438e5b99b01b1c80d88e05d7b5589
masterasia1 <a.mayfield18@gmail.com>
2022-04-20 08:34:17 -0700

    added cards and CSS

--- src/Components/CardItem.js
+++ src/Components/CardItem.js
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 function CardItem(props) {
   return (
     <>
-    <li className='cards_item'>
-        <Link className='cards_item_link' to={props.path}>
+    <li className='cards__item'>
+        <Link className='cards__item__link' to={props.path}>
 
-            <figure className='cards_item_pic-wrap' data-category={props.label}>
-                <img src={props.src} alt='Van Life' className='cards_item_img' />
+            <figure className='cards__item__pic-wrap' data-category={props.label}>
+                <img src={props.src} alt='Van Life' className='cards__item__img' />
             </figure>
-            <div className='cards_item_info'>
-                <h5 className='cards-item_Text'>
+            <div className='cards__item__info'>
+                <h5 className='cards__item__text'>
                     {props.text}
                 </h5>
 
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -2,20 +2,27 @@ import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css'
 
+
 function Cards() {
   return (
     <div className='cards'>
-        <h1>Van Life Can take You Anywhere</h1>
-        <div className='cards_container'>
-            <div className='cards_wrapper'>
-                <ul className='cards_items'>
+        <h1>Van Life Can Take You Anywhere</h1>
+        <div className='cards__container'>
+            <div className='cards__wrapper'>
+                <ul className='cards__items'>
                     <CardItem 
-                    src='images/img-3'
+                    src= 'images/img-3.jpg'
                     text='Drive through the desert'
                     label='Adventure'
-                    path='/services'
+                    path='/locations'
                     />
                 </ul>
+                <CardItem 
+                    src= 'images/img-9.jpg'
+                    text='Experience the sea'
+                    label='Travel'
+                    path='/locations'
+                    />
 
             </div>
         </div>
